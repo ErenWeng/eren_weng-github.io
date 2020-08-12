49071d4ba8c0e760135ddc7340ebe5bdc50a14b2
ErenWeng <f245986@gmail.com>
2020-08-13 00:32:16 +0800

    prepare for the demo

---
+++ demo/script.js
@@ -0,0 +1,68 @@
+window.onload = function() {
+  let navTopTrigger = document.querySelector('#mobile-nav-top-trigger');
+  let navTop = document.querySelector('#mobile-nav-top');
+
+  navTopTrigger.addEventListener('click', () => {
+      if(navTopTrigger.classList.contains('active')) {
+          navTopTrigger.classList.remove('active')
+          navTop.classList.remove('open')
+      } else {
+          navTopTrigger.classList.add('active')
+          navTop.classList.add('open')
+      }
+  });
+
+  let navBtnTrigger = document.querySelector('#mobile-nav-btn-trigger');
+  let navBtn = document.querySelector('#mobile-nav-btn');
+
+  navBtnTrigger.addEventListener('click', () => {
+    if(navBtnTrigger.classList.contains('active')) {
+        navBtnTrigger.classList.remove('active')
+        navBtn.classList.remove('open')
+    } else {
+        navBtnTrigger.classList.add('active')
+        navBtn.classList.add('open')
+    }
+  });
+
+  let call = document.querySelector('.fixed_call')
+  let change = document.querySelector('.fixed_call_change')
+
+  call.addEventListener('click', () => {
+    change.style.width = "280px";
+    change.style.height = "215px";
+    change.style.zIndex = "2";
+    change.style.borderRadius = "1%";
+    change.style.transition = ".3s";
+    change.style.opacity = "1";
+  })
+
+  let closeInfo = document.querySelector('#info_close')
+
+  closeInfo.addEventListener('click', () => {
+    change.style.width = "80px";
+    change.style.height = "80px";
+    change.style.zIndex = "0";
+    change.style.borderRadius = "50%";
+    change.style.transition = ".3s";
+    call.style.opacity = "1";
+  })
+
+  let toTop = document.querySelector('.to_top')
+
+    window.onscroll = function scroll(){
+        let y = window.pageYOffset;
+        if (y > 300){
+            toTop.style.display = "flex";
+        }else{
+            toTop.style.display = "none";
+        }
+    }
+
+    toTop.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    })
+};
